2615069ade61e6c095efc429f2400153ad7d27d3
AnthonyH16 <132026902+AnthonyH16@users.noreply.github.com>
2023-07-11 18:50:24 -0400

    test

--- rps.js
+++ rps.js
@@ -3,12 +3,9 @@ function getComputerChoice() {
     return array[Math.floor(Math.random() * array.length)];
  
 }
-function playRound(){
 let compSelection = getComputerChoice();
 let playerSelection = prompt("Rock, Paper, or Scissors?")
-console.log("You chose " + playerSelection);
-    console.log("Computer chose " + compSelection);
+
+function playRound(){  //this is one round of Rock Paper Scissors
+    test
 }
-playRound();
-playRound();
-playRound();
